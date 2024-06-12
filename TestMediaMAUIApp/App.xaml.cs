90e6bfcb859d9e800998d30b353f018152b11594
Niels Linssen <niels.linssen@any2info.com>
2024-06-12 15:20:33 +0200

    initial commit

---
+++ TestMediaMAUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestMediaMAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TestMediaMAUIApp/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMediaMAUIApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
